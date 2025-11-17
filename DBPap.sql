e85d3d22ff1de8a978aaec3c16ed899895f96fdd
devthony <104657459+anthonyfuzinato@users.noreply.github.com>
2025-11-17 22:00:43 +0000

    DBPap.sql

--- DBPap.sql
+++ DBPap.sql
@@ -1,4 +1,4 @@
-﻿create table Utilizador(
+create table Utilizador(
 	nif char(9) not null unique primary key,
 	email varchar(100) not null unique primary key check(email like'%@%.%'),
 	cod_utilizador char(3) check(cod_utilizador in('a','p','adm')),
@@ -25,7 +25,7 @@ create table aluno(
 		),
 );
 
-create table personaltrainer (
+create table PT (
     
     cod_pt char(6) not null primary key check (cod_pt like 'P[0-9][0-9][0-9][0-9][0-9]'),
     nif varchar(35) not null references utilizador(nif),
@@ -38,13 +38,13 @@ create table personaltrainer (
 );
 
 
-create table admin(
+create table Administrador(
 	id_admin int identity(0,1) primary key,
 	nif char(9) references utilizador(nif),
 	super bit default 0
 );
 
-create table treino (
+create table Treino (
     id_treino int not null identity(1,1) primary key,
     nif char(6) not null references aluno(nif),  
     data_treino date default getdate() check (data_treino <= getdate()),
@@ -53,7 +53,7 @@ create table treino (
     treino_personalizado bit default 0
 );
 
-create table exercicio (
+create table Exercicio (
     id_exercicio int not null identity(1,1) primary key,
     designacao varchar(100) not null,
     repeticoes smallint not null,
@@ -62,7 +62,7 @@ create table exercicio (
     peso decimal(5,2)
 );
 
-create table conquista (
+create table Conquista (
     id_conquista int not null identity(1,1) primary key,
     nome varchar(100) not null,
     descricao text not null,
@@ -70,6 +70,85 @@ create table conquista (
     nivel_conquista varchar(7) not null check (nivel_conquista in ('ouro', 'prata', 'bronze'))
 );
 
+create table Ginasio (
+    id_ginasio int identity(0,1) not null primary key,
+    nome varchar(100) not null,
+    localizacao varchar(250) not null
+)
+
+create table Plano_personalizado(
+    id_plano int identity(0,1) not null primary key,
+    objetivo varchar (150),
+    nivel char(1) check (nivel between 1 and 3),
+    duracao int check (duracao >0),
+    descricao text not null
+)
+
+create table Exercicio_Treino(
+    id_exercicio_treino int identity(0,1) primary key,
+    id_exercicio references Exercicio(id_exercicio),
+    id_treino references Treino(id_treino)
+)
+
+create table Plano_treino_Aluno (
+    id_t_a int identity(0,1),
+    id_plano int references Plano_personalizado(id_plano),
+    nif char(6) not null references aluno(nif),
+    dta_inicio date,
+    dta_fim date
+)
+
+create table Aluno_Conquista(
+    id_aluno_conquista int identity(0,1) primary key, 
+    id_conquista references Conquista(id_conquista),
+    nif char(6) not null references aluno(nif),
+    dta date
+)
+
+create table Pt_Ginasio(
+    id_pt_ginasio int identity(0,1) primary key,
+    nif char(6) not null references aluno(nif),
+    id_ginasio int references Ginasio(id_ginasio),
+    data_admissao date
+)
+
+create table Treino_Plano(
+    id_treino_plano int identity(0,1) primary key,
+    id_plano int references Plano_personalizado(id_plano),
+    id_treino int references Treino(id_treino),
+    feito_correto bit default 0
+)
+
+create table Adm_Ginasio(
+    id_adm_ginasio int identity (0,1) primary key,
+    nif char(6) not null references aluno(nif),
+    id_ginasio int references Ginasio(id_ginasio),
+    dta_atribuicao date,
+    permissao_total bit default 0
+)
+
+create table Especialidade(
+    id_especialidade int identity(0,1) primary key,
+    nome varchar(100) not null,
+    descricao text
+)
+
+create table Pt_Especialidade(
+    id_pt_especialidade int identity (0,1) primary key,
+    nif char(6) not null references aluno(nif),
+    id_especialidade int references Especialidade(id_especialidade),
+    certificado bit default 0,
+    data_certificacao date
+)
+
+create table Nome_exercicios(
+    id_nome_exercicio int identity(0,1) primary key,
+    nome varchar(50) not null,
+    grupo_muscular varchar(50) not null,
+    equipamento varchar(50),
+    descricao text not null,
+    dificuldade varchar(11) not null check (dificuldade in ('fácil','intermédio','dificil'))
+)
 
 create trigger Atualizar_IMC_Automatico
 on aluno
